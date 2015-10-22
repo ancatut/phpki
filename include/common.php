f856b58cd431b637c3778934b70a7c29345dd2fd
interiorcodealligator <root@hiwilinux-384>
2015-10-22 15:43:50 +0200

    Fixed directory path and variables referring to OpenVPN config

--- include/common.php
+++ include/common.php
@@ -128,7 +128,7 @@ function printFooter() {
 	<br>
 	<hr width="99%" align="left" color="#99caff">
 	<p style='margin-top: -5px; font-size: 8pt; text-align: center'>Based on PHPki <a href="http://sourceforge.net/projects/phpki/">v<?=PHPKI_VERSION?></a> - Copyright 2003 - William E. Roadcap</p>
-	<p style='margin-top: -5px; font-size: 8pt; text-align: center'>Current version of update branch on GitHub: <a href="https://github.com/interiorcodealligator/phpki/releases/tag/v0.11">v0.11</a></p>
+	<p style='margin-top: -5px; font-size: 8pt; text-align: center'>Current version of update branch on GitHub: <a href="https://github.com/interiorcodealligator/phpki/releases/tag/v0.11.1">v0.11.1</a></p>
 	</body>
 	</html>
 	<?php
